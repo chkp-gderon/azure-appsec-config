ad24e70f4cfbf722cfac035156e4643c7d96e6b5
chkp-gderon <29176555+chkp-gderon@users.noreply.github.com>
2023-02-08 11:52:29 +0100

    initial commit

---
+++ main.tf
@@ -0,0 +1,19 @@
+//********************** Provider Configuration **************************//
+
+terraform {
+  required_providers {
+    inext = {
+      source  = "CheckPointSW/infinity-next"
+      version = "1.0.3"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.56.0"
+    }
+  }
+}
+
+provider "inext" {
+  region = "eu"
+}
